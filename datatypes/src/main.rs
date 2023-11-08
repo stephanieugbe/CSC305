5808f97f900d5365cf90c07b76acc78e3107abc1
stephanieugbe <stephanie.ugbe@pau.edu.ng>
2023-11-08 15:24:18 +0100

    scalar commit

--- datatypes/src/main.rs
+++ datatypes/src/main.rs
@@ -1,16 +1,16 @@
-mod how_you_hold_data_for_operations;
+mod how_you_hold_data_foroperations;
 fn main() {
     println!("Hello, world!");
-    how_you_hold_data_for_operations::primitives::compound::array_example();
-    how_you_hold_data_for_operations::primitives::compound::slice_example();
-    how_you_hold_data_for_operations::primitives::compound::tuple_example();
-    how_you_hold_data_for_operations::primitives::scalar::get_integer();
-    how_you_hold_data_for_operations::primitives::scalar::is_true();
-    how_you_hold_data_for_operations::primitives::scalar::floating_point_types();
-    //how_you_hold_data_for_operations::primitives::scalar::never_type();
-    how_you_hold_data_for_operations::primitives::scalar::charact();
-    how_you_hold_data_for_operations::derived::user_defined::struct_example();
-    how_you_hold_data_for_operations::derived::user_defined::enum_example();
+    how_you_hold_data_foroperations::primitives::compound::array_example();
+    how_you_hold_data_foroperations::primitives::compound::slice_example();
+    how_you_hold_data_foroperations::primitives::compound::tuple_example();
+    how_you_hold_data_foroperations::primitives::scalar::get_integer();
+    how_you_hold_data_foroperations::primitives::scalar::is_true();
+    how_you_hold_data_foroperations::primitives::scalar::floating_point_types();
+    //how_you_hold_data_foroperations::primitives::scalar::never_type();
+    how_you_hold_data_foroperations::primitives::scalar::charact();
+    how_you_hold_data_foroperations::derived::user_defined::struct_example();
+    how_you_hold_data_foroperations::derived::user_defined::enum_example();
 
 
 }
---
+++ datatypes/target/debug/deps/datatypes.d
@@ -0,0 +1,11 @@
+C:\Users\Stephanie Ugbe\Documents\CSC305\datatypes\target\debug\deps\datatypes.exe: src\main.rs src\how_you_hold_data_foroperations.rs src\how_you_hold_data_foroperations\derived.rs src\how_you_hold_data_foroperations\derived\user_defined.rs src\how_you_hold_data_foroperations\primitives.rs src\how_you_hold_data_foroperations\primitives\compound.rs src\how_you_hold_data_foroperations\primitives\scalar.rs
+
+C:\Users\Stephanie Ugbe\Documents\CSC305\datatypes\target\debug\deps\datatypes.d: src\main.rs src\how_you_hold_data_foroperations.rs src\how_you_hold_data_foroperations\derived.rs src\how_you_hold_data_foroperations\derived\user_defined.rs src\how_you_hold_data_foroperations\primitives.rs src\how_you_hold_data_foroperations\primitives\compound.rs src\how_you_hold_data_foroperations\primitives\scalar.rs
+
+src\main.rs:
+src\how_you_hold_data_foroperations.rs:
+src\how_you_hold_data_foroperations\derived.rs:
+src\how_you_hold_data_foroperations\derived\user_defined.rs:
+src\how_you_hold_data_foroperations\primitives.rs:
+src\how_you_hold_data_foroperations\primitives\compound.rs:
+src\how_you_hold_data_foroperations\primitives\scalar.rs:
